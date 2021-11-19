2a33d03612e21bbb555b4157dc317bff9b7323c6
VivekCodes <51886845+vivek9520@users.noreply.github.com>
2021-11-19 16:37:58 +0530

    Update shopRoute.js
    changes

--- routes/shopRoute.js
+++ routes/shopRoute.js
@@ -1,6 +1,9 @@
 const { Router} = require('express');
 const shopController = require('../controllers/shopController')
 const router = Router();
+router.get("/", (req, res) => {
+  res.send("welcome");
+});
 router.get('/api/v1/shops', shopController.shops_get);
 router.get('/api/v1/shops/:id', shopController.shops_get_byID);
 router.post('/api/v1/shops',shopController.shop_post);
